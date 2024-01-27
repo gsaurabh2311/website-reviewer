a530236e8c0dbec4bfd601e03c40c0795b5a5d4e
Saurabh <gsaurabh.2311@gmail.com>
2024-01-27 18:13:40 +0530

    build-Fixes

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -3,32 +3,34 @@ import Logo from "../../public/assets/Logo.svg";
 import User from "../../public/assets/User.svg";
 import Menu from "../../public/assets/Menu.svg";
 export function Navbar() {
-    const navLinks = [
-        {name: 'Features'},
-        {name: 'Pricing'},
-        {name: 'Enterprise'},
-        {name: 'Careers'},
-    ]
+  const navLinks = [
+    { name: "Features" },
+    { name: "Pricing" },
+    { name: "Enterprise" },
+    { name: "Careers" },
+  ];
   return (
     <nav className="flex items-center w-full justify-between px-[20px] py-[16px] lg:container lg:mx-auto">
       <div className="flex gap-x-[74px]">
         <Image src={Logo} alt="Logo" />
         <div className="hidden lg:flex items-center gap-x-[56px]">
-            {
-                navLinks.map((item,idx)=> (
-                    <ul>
-                        <li className="font-medium text-[#36485C]" key={idx}>{item.name}</li>
-                    </ul>
-                ))
-            }
+          {navLinks.map((item, index) => (
+            <p className="text-[#36485C] font-medium" key={index}>
+              {item.name}
+            </p>
+          ))}
         </div>
       </div>
       <div className="flex gap-x-5">
-        <span className="hidden lg:block pr-[56px] text-[#36485C] font-medium">Open an Account</span>
+        <span className="hidden lg:block pr-[56px] text-[#36485C] font-medium">
+          Open an Account
+        </span>
         <div className="flex gap-x-[12px]">
-        <Image src={User} alt="User" />
-        <span className="hidden lg:block text-[#36485C] font-medium">Sign in</span>
-        <Image className="lg:hidden" src={Menu} alt="Menu" />
+          <Image src={User} alt="User" />
+          <span className="hidden lg:block text-[#36485C] font-medium">
+            Sign in
+          </span>
+          <Image className="lg:hidden" src={Menu} alt="Menu" />
         </div>
       </div>
     </nav>
